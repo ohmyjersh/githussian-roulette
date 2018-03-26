9b7c47d6356e32d8b6ea2bc6eb80ab8d27770a00
Josh Jarmain <jjarmain@gmail.com>
2018-03-26 06:49:03 -0700

    working on talking with github graphql

--- App.js
+++ App.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import {AuthSession} from 'expo';
 import { StyleSheet, Text, View, Button } from 'react-native';
-
+import { ApolloProvider } from 'react-apollo';
+import ApolloClient from 'apollo-boost';
+import Main from './Main';
 
 const GH_URL = 'https://github.com/login/oauth/authorize?scope=user:email'
 const CLIENT_ID = ''
@@ -15,9 +17,19 @@ export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Button title="Login with GitHub" onPress={this._handlePressAsync} />
+        {!this.state.result ? <Button class="" title="Login with GitHub" onPress={this._handlePressAsync} /> : null }
         {this.state.result ? (
-          <Text>{JSON.stringify(this.state.result)}</Text>
+          <ApolloProvider client={new ApolloClient({
+            uri: 'https://api.github.com/graphql',
+            fetchOptions: {
+              credentials: 'include',
+              headers: {
+                authorization: `Bearer ${this.state.result.params.code}`,
+              }
+            }
+          })}>
+          <Main />
+        </ApolloProvider>
         ) : null}
         </View>
     );
@@ -28,8 +40,10 @@ export default class App extends React.Component {
       authUrl:
         `${GH_URL}` +
         `&client_id=${CLIENT_ID}` +
-        `&redirect_uri=${encodeURIComponent(redirectUrl)}`,
+        `&redirect_uri=${encodeURIComponent(redirectUrl)}` +
+        `&scope=${encodeURIComponent(`user public_repo repo repo_deployment repo:status read:repo_hook read:org read:public_key read:gpg_key`)}`
     });
+    console.log(result.params.code);
     this.setState({ result });
   };
 }
---
+++ Main.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import { gql } from 'apollo-boost';
+import { Query } from 'react-apollo';
+
+const HelloWorld = gql`
+query queryIssue {
+    helpwanted: search( query:"label:help_wanted updated:>2017-04-30T00:00:00Z", type: ISSUE, first:1){
+        edges{
+        node{
+            ... on Issue{
+            title
+            url
+            labels(first:10) {
+                edges {
+                    node {
+                        name
+                        }
+                    }
+                }
+            repository {
+                name
+                    primaryLanguage {
+                        name
+                        }
+                    }
+                }
+            }
+        }
+    },
+}
+`
+export default class Main extends React.Component {
+  state = {
+
+  };
+  render() {
+    return (
+      <View>
+      <Query query={HelloWorld}>
+      {({ loading, error, data }) => {
+        if (loading) return <Text>Loading...</Text>;
+        if (error) {
+            console.log(error);
+             return <Text>Error :(</Text>; 
+            }
+  
+        return (
+          <Text>{JSON.stringify(data)}</Text>
+        )
+      }}
+    </Query>
+    </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
